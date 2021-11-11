cc89e0a9d5bb35d43443838a638241b7df5e0e06
adultmalehuman <adultmalehuman@gmail.com>
2021-11-11 13:26:49 -0400

    read/write high score to/from a text file

--- scoreboard.py
+++ scoreboard.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 
 ALIGNMENT = "center"
 FONT = ("Arial", 18, "normal")
+HIGH_SCORE_FILENAME = "data.txt"
 
 
 class Scoreboard(Turtle):
@@ -10,11 +11,21 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         self.high_score = 0
+        self.read_high_score()
         self.hideturtle()
         self.penup()
         self.goto(0, 270)
         self.color("white")
         self.update_scoreboard()
+
+    def read_high_score(self):
+        with open(HIGH_SCORE_FILENAME) as file:
+            high_score_text = file.read()
+            self.high_score = int(high_score_text)
+
+    def write_high_score(self):
+        with open(HIGH_SCORE_FILENAME, mode="w") as file:
+            file.write(f"{self.high_score}")
 
     def update_scoreboard(self):
         self.clear()
@@ -23,6 +34,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.write_high_score()
         self.update_scoreboard()
         self.score = 0
 
